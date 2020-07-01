9d74317893d4612016737abf97edc894de890ddf
enthusiasticgeek <enthusiasticgeek@gmail.com>
2020-07-01 10:59:13 -0400

    elements size of a tuple

--- c++/tuple.cpp
+++ c++/tuple.cpp
@@ -4,6 +4,7 @@ int main(int argc, char* argv[])
 {
 auto triple1 = std::make_tuple(5, 6, 7);
 std::tuple<int,int,int> triple = std::make_tuple(5, 6, 7);
+std::cout << "Tuple elements = " <<  std::tuple_size<decltype(t)>::value;
 std::cout << std::get<0>(triple);
 std::cout << std::get<1>(triple);
 return 0;
