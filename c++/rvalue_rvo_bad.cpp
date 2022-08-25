5811fb3bdbb3387f8d389c3c8c4591522323e49b
Pratik Tambe <enthusiasticgeek@gmail.com>
2022-08-24 21:15:33 -0400

    rvo optimization

---
+++ c++/rvalue_rvo_bad.cpp
@@ -0,0 +1,44 @@
+//https://www.scaler.com/topics/cpp/lvalue-and-rvalue/
+//!!! BAD CODE !!!
+
+#include <iostream>
+
+using namespace std;
+
+class A {
+    int *arr, size;
+    
+    public:
+    
+    A(int len) {
+        size = len;
+        arr = new int[len];
+    }
+    A(const A& temp) {
+        size = temp.size;
+        delete [] arr;
+        arr = new int[size];
+        for(int i=0;i<size;i++)
+            arr[i] = temp.arr[i];
+    }
+    A(A&& temp) {
+        size = temp.size;
+        arr = temp.arr;
+        temp.arr = nullptr;
+    }
+    ~A() {
+        delete [] arr;
+    }
+};
+
+A getObj() {
+    A obj(10);
+    return std::move(obj); //Return Value Optimization
+}
+
+int main()
+{
+    A obj = getObj();
+
+    return 0;
+}
