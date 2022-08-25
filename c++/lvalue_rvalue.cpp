80c448ac35dee32a9017fb377b5910b551fceeb2
Pratik Tambe <enthusiasticgeek@gmail.com>
2022-08-24 20:40:16 -0400

    rvalue_lvalue

---
+++ c++/lvalue_rvalue.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void func1(const int &x) {
+    cout<<"Inside lvalue func1 : x = "<<x<<endl;
+}
+
+void func1(int &&x) {
+    cout<<"Inside rvalue func1 : x = "<<x<<endl;
+}
+
+
+int main()
+{
+    int x = 10;
+    func1(x);
+    func1(15);
+    return 0;
+}
+
