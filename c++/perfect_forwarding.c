3f80f516f8808de1554f4e1ffa8c9ef51803f04c
enthusiastic geek <enthusiasticgeek@gmail.com>
2023-06-23 15:20:13 -0400

    Create perfect_forwarding.c

---
+++ c++/perfect_forwarding.c
@@ -0,0 +1,36 @@
+/*
+In this example, the process template function takes a forwarding reference (T&& arg), 
+and two overloaded versions of someFunction are defined, one for an lvalue reference and another for an rvalue reference.
+
+In main(), the process function is called with x, which is an lvalue. 
+The template deduction deduces T as int&, and arg becomes an lvalue reference. 
+As a result, when someFunction is called within process, the version accepting an lvalue reference is invoked.
+
+Next, process is called with the literal value 10, which is an rvalue. 
+This time, T is deduced as int, and arg becomes an rvalue reference. 
+Consequently, the version of someFunction accepting an rvalue reference is called within process.
+*/
+
+
+#include <iostream>
+#include <utility>
+
+template<typename T>
+void process(T&& arg) {
+    std::cout << "Processing: " << arg << std::endl;
+}
+
+void someFunction(int& value) {
+    std::cout << "Called by lvalue reference" << std::endl;
+}
+
+void someFunction(int&& value) {
+    std::cout << "Called by rvalue reference" << std::endl;
+}
+
+int main() {
+    int x = 25;
+    process(x); // lvalue passed, calls someFunction with lvalue reference
+    process(10); // rvalue passed, calls someFunction with rvalue reference
+    return 0;
+}
