8796322f67d9a93c7e9f4abe603ce67060629d0f
enthusiastic geek <enthusiasticgeek@gmail.com>
2023-06-26 00:30:50 -0400

    Update asyncio.py

--- python/asyncio.py
+++ python/asyncio.py
@@ -3,6 +3,7 @@ import asyncio
 
 
 async def greet(name):
+    #hello prints immediately but goodbye prints after asyncio
     print(f"Hello, {name}!")
     await asyncio.sleep(1)  # Simulate asynchronous I/O
     print(f"Goodbye, {name}!")
